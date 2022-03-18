e61df66e6ad9c072fdf62e42d5c23fe09e109af6
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-03-18 08:48:57 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 
 $dbhost = "localhost";
 $dbuser = "garconia_arinji";
-$dbpass = "SL4kwf05";
+$dbpass = "******";
 $dbname = "garconia_sample-login";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
